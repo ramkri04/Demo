e972a6c7e19844cd8a54f935d359f826723711b7
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-04 23:37:10 -0800

    Update Demo1.js

--- Demo1.js
+++ Demo1.js
@@ -17,7 +17,7 @@
     
     var _reactNative = ReactNative;
     var root = this;
-    return react_1.createElement(_reactNative.View, { style : { "backgroundColor":"lightgrey"}},
+    return react_1.createElement(_reactNative.View, { style : { "backgroundColor":"lightgrey",paddingTop:10}},
 
         [react_1.createElement(_reactNative.View,
 
