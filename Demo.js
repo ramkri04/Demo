357ef1870a37db22eec5c6c12231ed528d157df7
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-04 19:44:00 -0800

    Create Demo.js

---
+++ Demo.js
@@ -0,0 +1,25 @@
+(function main(React,ReactNative,componentState, require) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+  
+var _react = React;
+
+var _react2 = _interopRequireDefault(_react);
+
+  
+
+  
+var _reactNative = ReactNative;
+   
+  return _react2.default.createElement(
+          _reactNative.Text,
+          { onPress : function () { alert(componentState.state.existingUserId) }},
+          ['Nice Once Again. Thank you !!']
+      );
+      
+})
