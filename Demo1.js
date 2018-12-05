2c64dededebfd1dd001a7ef83af6a16d28bdfc04
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-04 23:49:39 -0800

    Update Demo1.js

--- Demo1.js
+++ Demo1.js
@@ -1,4 +1,4 @@
-(function main(React, ReactNative, componentState,Button,Platform require) {
+(function main(React, ReactNative, componentState,Button require) {
     'use strict';
 
     Object.defineProperty(exports, "__esModule", {
@@ -17,7 +17,7 @@
     
     var _reactNative = ReactNative;
     var root = this;
-    return react_1.createElement(_reactNative.View, { style : { "backgroundColor":"lightgrey",marginTop:Platform.OS === 'ios'?20:5}},
+    return react_1.createElement(_reactNative.View, { style : { "backgroundColor":"lightgrey",marginTop:_reactNative.Platform.OS === 'ios'?20:5}},
 
         [react_1.createElement(_reactNative.View,
 
