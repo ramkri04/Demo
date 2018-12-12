908010e391eaba6222f01665840ddf829f033bee
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-12 16:22:16 +0530

    Update loginDemo.js

--- loginDemo.js
+++ loginDemo.js
@@ -94,7 +94,7 @@
 }, [react_1.createElement(reactNative.Text, {
     "htmlFor": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl02",
     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl02_label"
-}, "Confirm User ID")]), react_1.createElement("TextInput", {
+}, "Confirm User ID")]), react_1.createElement(reactNative.TextInput, {
     "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl02",
     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl02",
     "cssClass": "form-control component-group",
