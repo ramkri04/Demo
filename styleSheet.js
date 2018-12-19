4d1fad86074e309413b37138f9758ebc543bbd0a
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-19 18:08:11 +0530

    Update styleSheet.js

--- styleSheet.js
+++ styleSheet.js
@@ -1,1 +1,53 @@
+(function main(React, ReactNative, componentState, Button, Footer, StyleSheet1, responsiveHeight, responsiveFontSize, require) {
+    
+    var react_1 = React;
 
+    var _reactNative = ReactNative;
+
+    var root = this;
+  var styles = StyleSheet1.create({
+  container: {
+    flex: 1,
+    padding : responsiveHeight(4)
+  },
+  loadingView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: responsiveHeight(2)
+  },
+  saveButton: {
+    fontSize: responsiveFontSize(2),
+    fontWeight: "normal",
+    padding: responsiveHeight(0.5),
+    borderWidth: responsiveHeight(0.1),
+    borderColor: "#0061b8", backgroundColor: "white",
+    marginTop:responsiveHeight(2)
+  },
+  cancelButton: {
+    fontSize: responsiveFontSize(2),
+    fontWeight: "normal",
+    padding: responsiveHeight(0.5),
+    marginTop:responsiveHeight(6),
+    color: "white",
+    borderWidth: responsiveHeight(0.1),
+    borderColor: "#015EBF", backgroundColor: "#0061b8"
+  },
+  padding: {
+    padding: responsiveHeight(2),
+  },
+  textInput : {
+    borderWidth: 0.9, padding: 0, 
+    backgroundColor: "white", 
+    borderColor: "gray",
+    marginTop:responsiveHeight(1)
+  },
+  labelMargin : {
+   marginTop:responsiveHeight(3),
+   color:"gray"
+  }
+});
+
+});
+    
