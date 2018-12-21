9536d53a260b07b6e836b5a0f67a679826e09ec9
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-21 11:03:06 +0530

    Update styleSheet.js

--- styleSheet.js
+++ styleSheet.js
@@ -4,7 +4,7 @@
     var _reactNative = ReactNative;
     var root = this;
      
-  var styles =StyleSheet.create({
+  /*var styles =StyleSheet.create({
    container: {
      flex: 1,
      padding : responsiveHeight(4)
@@ -46,7 +46,49 @@
    marginTop:responsiveHeight(3),
    color:"gray"
   }
-});
-return react_1.createElement(_reactNative.createElement(styles));
+}); */
+return react_1.createElement(_reactNative.StyleSheet.create({
+   container: {
+     flex: 1,
+     padding : responsiveHeight(4)
+  },
+  loadingView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: responsiveHeight(2)
+  },
+  saveButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: responsiveHeight(0.5),
+    borderWidth: responsiveHeight(0.1),
+    borderColor: "#0061b8", backgroundColor: "white",
+    marginTop:responsiveHeight(2)
+  },
+  cancelButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: responsiveHeight(0.5),
+    marginTop:responsiveHeight(6),
+    color: "white",
+    borderWidth: responsiveHeight(0.1),
+    borderColor: "#015EBF", backgroundColor: "#0061b8"
+  },
+  padding: {
+    padding: responsiveHeight(2),
+  },
+  textInput : {
+    borderWidth: 0.9, padding: 0, 
+    backgroundColor: "white", 
+    borderColor: "gray",
+    marginTop:responsiveHeight(1)
+  },
+  labelMargin : {
+   marginTop:responsiveHeight(3),
+   color:"gray"
+  }
+}));
 });
     
