56a20fa962731249c48ef294129ad22b0ded25b8
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-24 10:50:15 +0530

    Update styleSheet.js

--- styleSheet.js
+++ styleSheet.js
@@ -4,7 +4,7 @@
     var _reactNative = ReactNative;
     var root = this;
      
-return react_1.createElement({
+return react_1.createElement(StyleSheet.create({
    container: {
      flex: 1,
      padding : 4
@@ -46,7 +46,7 @@ return react_1.createElement({
    marginTop:3,
    color:"gray"
   }
-});
+}));
     
 });
     
