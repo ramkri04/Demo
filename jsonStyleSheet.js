3cbb04ee32c88cc49b7179d9f99db659a49c4e70
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2018-12-26 18:10:25 +0530

    Update jsonStyleSheet.js

--- jsonStyleSheet.js
+++ jsonStyleSheet.js
@@ -1,1 +1,43 @@
-
+{
+   container: {
+     flex: 1,
+     padding : 4
+  },
+  loadingView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 2
+  },
+  saveButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: 0.5,
+    borderWidth:0.1,
+    borderColor: "#0061b8", backgroundColor: "white",
+    marginTop:2
+  },
+  cancelButton: {
+    fontSize: 12,
+    fontWeight: "normal",
+    padding: 0.5,
+    marginTop:6,
+    color: "white",
+    borderWidth: 0.1,
+    borderColor: "#015EBF", backgroundColor: "#0061b8"
+  },
+  padding: {
+    padding: 2,
+  },
+  textInput : {
+    borderWidth: 0.9, padding: 0, 
+    backgroundColor: "white", 
+    borderColor: "gray",
+    marginTop:1
+  },
+  labelMargin : {
+   marginTop:3,
+   color:"gray"
+  }
+}
