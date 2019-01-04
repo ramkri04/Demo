aa2328e2d5f955b60d8d8fbaa8ed3566bbd3721e
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2019-01-04 11:24:11 +0530

    Rename loginDemo.js to loginStyle.js

---
+++ loginStyle.js
@@ -0,0 +1,63 @@
+(function main(StyleSheet) {
+
+    return StyleSheet.create({
+        containerStyle: {
+            height: '100%',
+            width: '100%',
+            backgroundColor: 'transparent',
+        },
+        viewStyle: {
+            marginTop: "20%",
+            height: Dimensions.get('window').height / 3
+        },
+        userNameItem: {
+            width: "90%",
+            marginLeft: "5%",
+            marginRight: "5%",
+        },
+        inputStyle: {
+            color: '#2B7B9F',
+            paddingLeft: 10,
+            fontSize: 24,
+            fontWeight: '500',
+            width: '100%',
+            height: 50,
+            borderWidth: 1,
+            //textTransform: 'none',
+            borderColor: '#ccc',
+        },
+        passwordItem: {
+            width: "90%",
+            marginLeft: "5%",
+            marginRight: "5%",
+            marginTop: "5%",
+        },
+        passwordlabel: {
+            color: '#2B7B9F',
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            marginBottom: 5,
+            fontSize: 16
+        },
+        userNameLabel: {
+            color: '#2B7B9F',
+            fontFamily: 'Arial',
+            fontWeight: 'bold',
+            marginBottom: 5,
+            fontSize: 16
+        },
+        loginButton: {
+            backgroundColor: '#4568DC',
+            marginTop: '3%',
+            width: '90%',
+            marginLeft: '5%',
+            marginRight: '5%',
+            marginTop: 40,
+        },
+        imageStyle: {
+            width: '100%',
+            height: 160
+        }
+    });
+
+});
