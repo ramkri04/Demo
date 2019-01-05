6b1bba049feeac2d5483690e8932806f2ebea531
ramkri04 <45609714+ramkri04@users.noreply.github.com>
2019-01-05 11:53:42 +0530

    Update login.js

--- login.js
+++ login.js
@@ -15,11 +15,11 @@
                         "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_field_container",
                         style:styles.viewStyle
                     }, [
-                            react_1.createElement(_reactNative.ImageBackground,{
+                            react_1.createElement(_reactNative.Image, {
                                     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl0423_Label_container",
                                     style:styles.imageStyle,
-                                    source:{logo}
-                            },[]),
+                                    source:{uri:'https://raw.githubusercontent.com/ramkri04/Demo/master/Mainlog.png'}
+                            }, null),
                             react_1.createElement(_reactNative.View, {
                                     "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_Label_container",
                                     style:styles.userNameItem
